278388e51a12e04246293a3771cfe104e0fcbd55
Lyubomyr Boychuk <lyuboychuk@gmail.com>
2026-01-13 21:28:08 +0100

    added comments to the code

--- lib/debounce/debounce.h
+++ lib/debounce/debounce.h
@@ -10,7 +10,12 @@
 
 #define LED_PIN1 18
 #define BTN_PIN1 0
-#define MODULE_2_6_T4
+
+/* Only one maybe uncommented at a time*/
+// #define MODULE_2_6_T1 //- task 1 for 2.6 module
+#define MODULE_2_6_T2 //- task 2 for 2.6 module
+// #define MODULE_2_6_T3 //- task 3 for 2.6 module
+// #define MODULE_2_6_T4 //- task 3 for 2.6 module    
 
 extern const char *TAG;
 extern void btn_pressed_hdlr(void *arg);
